48c731b96f2c75f5c2ec3eacca3e2ada2ab70a5c
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2013-12-07 13:09:54 +0100

    Fix pulseaudio volume matching

--- delightful/widgets/pulseaudio.lua
+++ delightful/widgets/pulseaudio.lua
@@ -203,7 +203,7 @@ function update_data(force_update)
 					end
 			end)
 			-- parse volume
-			line:gsub('^%s+volume:%s+0:%s+(%d+)%%.*$', function(match)
+			line:gsub('^%s+volume:%s+front%-left:%s+%d+%s+/%s+(%d+)%%.*$', function(match)
 					if not sink_num_ok then
 						return
 					end
